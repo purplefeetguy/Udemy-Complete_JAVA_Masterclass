b8296de0a9f2399bf9026911525d8188170a688d
purplefeetguy <purplefeetguy@hotmail.com>
2018-02-03 19:42:22 -0600

    finished with shorts and longs

--- ByteShortInt/src/com/company/Main.java
+++ ByteShortInt/src/com/company/Main.java
@@ -4,22 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // int hwas a width of 32
-	    int myMinValue = -2_147_483_648;
-        int myMaxValue = 2_147_483_647;
-        int myTotal = (myMinValue/2);
-        System.out.println("myTotal = " + myTotal);
+        // 1. Create a byte variable and set it to any valid byte number
+        // 2. Create a short variable and set it to any valid short number.
+        // 3. Create an int variable and set it to any valide int number.
+        // 4. Create a variable of type long, and make it equal to
+        //    50000 + 10 times the sum of the byte, plus the short plus the int
 
-        // byte has a width of 8
-        byte myByteValue = -128;
-        byte myNewByteValue = (byte) (myByteValue/2);
-        System.out.println("myNewByteValue = " + myNewByteValue);
+        byte byteValue = 10;
+        short shortValue = 20;
+        int intValue = 50;
 
-        //short has a width of 16
-        short myShortValue = 32767;
-        short myNewShortValue = (short) (myShortValue / 2);
+        long longTotal = 50000L + 10L * (byteValue + shortValue + intValue);
+        short shortTotal = (short) (1000 + 10 * (byteValue + shortValue + intValue));
+        System.out.println("longTotal = " + longTotal);
+        System.out.println("shortTotal = " + shortTotal);
 
-        // long has a width of 64
-        long myLongValue = 9_223_372_036_854_775_807L;
+
     }
 }
